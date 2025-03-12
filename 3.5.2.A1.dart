ac7b79653358b2a61031064453af5b91c201ffea
Grace <grace6424@icloud.com>
2025-03-12 11:21:19 +0100

    3.5.2.A1

---
+++ 3.5.2.A1.dart
@@ -0,0 +1,15 @@
+import 'functions.dart';
+
+void printNumber (int numberInput){
+print(numberInput);
+}
+
+void main() {
+
+  clearConsole();
+  printNumber(3);
+  printNumber(6);
+  printNumber(9);
+  lineBreak();
+
+}
